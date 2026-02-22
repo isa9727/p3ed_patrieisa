9b826021482aabfd7ee491d60ee68e04671a6eb9
PatrieIsa <manitta9727@gmail.com>
2026-02-23 00:00:45 +0100

    Se añade comentario en Persona

--- src/pruebapersona/Persona.java
+++ src/pruebapersona/Persona.java
@@ -1,5 +1,43 @@
+
+// Comentario añadido de prueba añadido
+// IES JUAN DE LA CIERVA DAM1D PATRICIA E ISABEL
+
 package pruebapersona;
 
 public class Persona {
 
+    private String nombre;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void saludar() {
+        System.out.println("Hola, me llamo " + nombre);
+    }
+
+    public void cumplirAnios() {
+        edad++;
+    }
 }
